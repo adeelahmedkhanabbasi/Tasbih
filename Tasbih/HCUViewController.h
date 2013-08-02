05b3bd2acc795525585d708138a11bcf633e3941
hanief <hanief@gmail.com>
2013-08-02 10:28:39 +0700

    initial ios app

--- Tasbih/HCUViewController.h
+++ Tasbih/HCUViewController.h
@@ -9,5 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface HCUViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *counterLabel;
+@property (weak, nonatomic) IBOutlet UISlider *amountSlider;
+@property (weak, nonatomic) IBOutlet UITextField *amountTextField;
+- (IBAction)resetButtonPressed:(id)sender;
+- (IBAction)upButtonPressed:(id)sender;
+- (IBAction)downButtonPressed:(id)sender;
+- (IBAction)sliderValueChanged:(UISlider*)sender;
 
 @end
--- Tasbih/HCUViewController.m
+++ Tasbih/HCUViewController.m
@@ -18,12 +18,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.amountSlider.value = 33;
+    self.counterLabel.text = self.amountTextField.text = @"33";
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)resetButtonPressed:(id)sender {
+    self.counterLabel.text = @"0";
+}
+
+- (IBAction)upButtonPressed:(id)sender {
+    if ([self.counterLabel.text integerValue] < [self.amountTextField.text integerValue]) {
+        self.counterLabel.text = [NSString stringWithFormat:@"%d",[self.counterLabel.text integerValue]+1];
+    }
+}
+
+- (IBAction)downButtonPressed:(id)sender {
+    if ([self.counterLabel.text integerValue] > 0) 
+        self.counterLabel.text = [NSString stringWithFormat:@"%d",[self.counterLabel.text integerValue]-1];
+}
+
+- (IBAction)sliderValueChanged:(UISlider*)sender {
+    int rounded = sender.value;
+    [sender setValue:rounded animated:NO];
+    self.amountTextField.text = [NSString stringWithFormat:@"%d",rounded];
 }
 
 @end
